d6a86f52a8889e571aef99d102194a79ffea325b
Srikrishna Valepe <srikrishna.valepe@gmail.com>
2024-10-29 16:32:44 +0530

    prediction function done, need to make minor changes

--- main.c
+++ main.c
@@ -2,11 +2,49 @@
 #include <stdlib.h>
 #include <string.h>
 #include "splay_tree.h"
-#include "plot.h"
+
+// Function to convert date string (MM/DD/YYYY) to an integer for regression
+int dateToInt(char* date) {
+    int month, day, year;
+    sscanf(date, "%d/%d/%d", &month, &day, &year);
+    return year * 10000 + month * 100 + day;
+}
+
+// Function to perform linear regression and predict the price for a future date
+float predictPrice(char dates[][11], float prices[], int size, char* futureDate) {
+    int *x = malloc(size * sizeof(int)); // Store dates as integers
+    float *y = prices;
+
+    // Convert dates to integers
+    for (int i = 0; i < size; i++) {
+        x[i] = dateToInt(dates[i]);
+    }
+
+    // Linear regression calculations
+    float sumX = 0, sumY = 0, sumXY = 0, sumX2 = 0;
+    for (int i = 0; i < size; i++) {
+        sumX += x[i];
+        sumY += y[i];
+        sumXY += x[i] * y[i];
+        sumX2 += x[i] * x[i];
+    }
+
+    float slope = (size * sumXY - sumX * sumY) / (size * sumX2 - sumX * sumX);
+    float intercept = (sumY - slope * sumX) / size;
+
+    // Convert the future date to an integer
+    int futureX = dateToInt(futureDate);
+
+    // Predict price
+    float predictedPrice = slope * futureX + intercept;
+
+    free(x);
+    return predictedPrice;
+}
 
 int main() {
     int n;
-    printf("Enter the number of days data is to be analyzed: \n");
+    printf("Enter the number of days data to analyze: ");
     scanf("%d", &n);
 
     char date[n][11];
@@ -53,6 +91,14 @@ int main() {
             printf("Date %s not found in the splay tree.\n", date[i]);
         }
     }
+
+    // Prediction for a future date
+    char futureDate[11];
+    printf("Enter the future date (MM/DD/YYYY) for prediction: ");
+    scanf("%10s", futureDate);
+
+    float predictedPrice = predictPrice(date, price, n, futureDate);
+    printf("Predicted price for %s: %.2f\n", futureDate, predictedPrice);
 
     return 0;
 }
