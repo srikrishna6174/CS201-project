01567780a618feaef86b07fd22c346b8a6757c08
prathvik1246 <142573079+prathvik1246@users.noreply.github.com>
2024-11-03 01:18:16 +0530

    Update splay_tree.h
    Updated the code

--- splay_tree.h
+++ splay_tree.h
@@ -4,6 +4,7 @@
 typedef struct Node {
     char date[11]; // Format: MM/DD/YYYY
     float price;
+    int volume;
     struct Node* left;
     struct Node* right;
 } Node;
@@ -19,20 +20,14 @@ extern struct Node* root;
 
 // Splay tree functions
 Node* splay(Node* root, char* date);
-Node* insert(Node* root, char* date, float price);
-
-// splay_tree.h
-
-// Other declarations...
-
+Node* insert(Node* root, char* date, float price ,int volume);
 int dateToDays(char* date);
-void batchInsert(Node** root, char dates[][11], float prices[], int size);
-//Stock* update_stock_price(Stock* root, char* symbol, float price);
-
+void batchInsert(Node** root, char dates[][11], float prices[], int volume[], int size);
 float searchByDate(Node** root, char* date);
-void printNodes(const NodeArray* arr);  // Helper function to display tree
+void printNodes(const NodeArray* arr);
 void initNodeArray(NodeArray* arr, size_t initialCapacity);
 void freeNodeArray(NodeArray* arr);
 void inOrder(Node* root, NodeArray* arr);
-float predictPrice(NodeArray* nodeArray, char* futureDate) ;
+float predictPrice(NodeArray* nodeArray, char* futureDate);
+double calculateMSE(NodeArray* nodeArray);
 #endif
